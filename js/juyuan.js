830fbfb8e62650cba9d3d205e93b65fdc420ed02
大超 <44048702+Generalchao@users.noreply.github.com>
2023-05-04 09:26:20 +0800

    first commit

---
+++ js/juyuan.js
@@ -0,0 +1,21 @@
+var app = new Vue({
+  el: '#nav',
+  data() {
+      return {
+          isNavTransparent: true
+      }
+  },
+  mounted() {
+      window.addEventListener('scroll', this.handleScroll)
+  },
+  destroyed() {
+      window.removeEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+      handleScroll() {
+          const scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+          this.isNavTransparent = scrollTop < 50 // 假设阈值为100，即滚动距离小于100时导航栏为透明
+      }
+  }
+
+})
